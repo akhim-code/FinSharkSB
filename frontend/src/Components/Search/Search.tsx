fd58b0203d62fd448d3028ff51625a029d55934a
akhim-code <albertkhim928@gmail.com>
2024-04-12 09:08:11 +0200

    State

---
+++ frontend/src/Components/Search/Search.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+type Props = {}
+
+const Search: React.FC<Props> = (props: Props): JSX.Element => {
+    const [search, setSearch] = React.useState<string>('')
+
+    const onClick = (e:any) => {
+        setSearch(e.target.value)
+        console.log(search)
+    }
+
+  return (
+    <div>
+        <input value={search} onChange={(e)=>onClick(e)}></input>
+    </div>
+  )
+}
+
+export default Search
