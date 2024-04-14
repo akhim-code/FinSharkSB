7d86a30f390e1581df91521730ce2d6732d17d84
akhim-code <albertkhim928@gmail.com>
2024-04-14 10:06:17 +0200

    Type narrowing + .env into root directory

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,18 +2,31 @@ import { ChangeEvent, SyntheticEvent, useState } from 'react';
 import './App.css';
 import CardList from './Components/CardList/CardList';
 import Search from './Components/Search/Search';
+import { searchCompanies } from './api';
+import { CompanySearch } from './company';
 
 function App() {
   const [search, setSearch] = useState<string>("");
+  const [searchResult, setSearchResult] = useState<CompanySearch[]>();
+  const [serverError, setServerError] = useState<string | null>(null);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
     console.log(e);
   };
 
-  const onClick = (e: SyntheticEvent) => {
-    console.log(e);
-  };
+  const onClick = async (e: SyntheticEvent) => {
+    const result = await searchCompanies(search);
+    if (typeof result === "string") {
+      setServerError(result);
+      console.log(serverError);
+    } 
+    else if (Array.isArray(result.data)) {
+      setSearchResult(result.data);
+    }
+    console.log(searchResult);
+  }
+  
   return (
     <div className="App">
       <Search onClick={onClick} search={search} handleChange={handleChange} />
--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,15 +1,14 @@
 import axios from "axios";
 import { CompanySearch } from "./company";
 
-interface SearchResponse {
+export interface SearchResponse {
     data: CompanySearch[];
 }
 
 export const searchCompanies = async (query: string) => {
     try {
         const data = await axios.get<SearchResponse>(
-            `https://financialmodelingprep.com/api/v3/search?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`
-        );
+            `https://financialmodelingprep.com/api/v3/search?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`        );
         return data;
     }
     catch (error) {
@@ -23,3 +22,4 @@ export const searchCompanies = async (query: string) => {
         }
     }
 };
+
