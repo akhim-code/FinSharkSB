bde68fca95507581f3f632f34f50cfaf4e7d636c
akhim-code <albertkhim928@gmail.com>
2024-04-22 08:30:22 +0200

    UseEffect for company profiles

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { CompanySearch } from "./company";
+import { CompanyProfile, CompanySearch } from "./company";
 
 export interface SearchResponse {
     data: CompanySearch[];
@@ -8,7 +8,8 @@ export interface SearchResponse {
 export const searchCompanies = async (query: string) => {
     try {
         const data = await axios.get<SearchResponse>(
-            `https://financialmodelingprep.com/api/v3/search?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`        );
+            `https://financialmodelingprep.com/api/v3/search?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`        
+        );
         return data;
     }
     catch (error) {
@@ -22,4 +23,16 @@ export const searchCompanies = async (query: string) => {
         }
     }
 };
+
+
+export const getCompanyProfile = async (query: string) => {
+    try {
+      const data = await axios.get<CompanyProfile[]>(
+        `https://financialmodelingprep.com/api/v3/profile/${query}?apikey=${process.env.REACT_APP_API_KEY}`
+      );
+      return data;
+    } catch (error: any) {
+      console.log("error message: ", error.message);
+    }
+  };
 
