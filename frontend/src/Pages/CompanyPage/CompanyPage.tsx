425ca34637bfaaacf8b06b63fdc0b7cb485ace4f
akhim-code <albertkhim928@gmail.com>
2024-04-27 08:39:35 +0200

    Dashboard V2

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -10,34 +10,8 @@ interface Props {}
 
 const CompanyPage = (props: Props) => {
   let { ticker } = useParams();
-  const tabItems = [
-    {
-      id: 1,
-      title: "Company Profile",
-      icon: "fas fa-child",
-      content: "step 1 content",
-    },
-    {
-      id: 2,
-      title: "Income Statment",
-      icon: "fas fa-users",
-      content: "step 2 content",
-    },
-    {
-      id: 3,
-      title: "Balance Sheet",
-      icon: "fas fa-network-wired",
-      content: "step 3 content",
-    },
-    {
-      id: 4,
-      title: "Cash Flow",
-      icon: "fa money-check-alt",
-      content: "step 4 content",
-    },
-  ];
+  
   const [company, setCompany] = useState<CompanyProfile>();
-  const [activeSidebarItem, setActiveSideBarItem] = useState<number>(1);
 
   useEffect(() => {
     const getProfileInit = async () => {
@@ -51,19 +25,9 @@ const CompanyPage = (props: Props) => {
     <>
       {company ? (
         <div className="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden">
-          <Sidebar
-            tabItems={tabItems}
-            setActiveSideBarItem={setActiveSideBarItem}
-            activeSidebarItem={activeSidebarItem!}
-          />
-          <CompanyDashboard
-            tabItems={tabItems}
-            activeSidebarItem={activeSidebarItem!}
-          >
+          <Sidebar />
+          <CompanyDashboard>
             <Tile title="Company Name" subTitle={company.companyName} />
-            <Tile title="DCF" subTitle={company.dcf.toString()} />
-            <Tile title="Dividend" subTitle={company.lastDiv.toString()} />
-            <Tile title="Price" subTitle={company.price.toString()} />
           </CompanyDashboard>
         </div>
       ) : (
